c40f2436260b9e1a05de36571f0743b05dd7bf93
OMJS2769 <mauricio2769@outlook.es>
2024-12-09 17:41:37 -0600

    integrar biblioteca numeros a texto

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use NumberToWords\NumberToWords;
 
 Auth::routes(['register' => false]);
 Route::group(['middleware' => ['auth']], function () {
     Route::view('home', 'home')->name('home');
 });
-Route::any('/', function () {
-})->name('/');
+Route::any('/', function () {})->name('/');
 Route::get('/', function () {
     if (Auth::check()) {
         return redirect('home');
@@ -17,8 +17,10 @@ Route::get('/', function () {
 })->name('/');
 
 
-// Route::get('test_ws', function () {
+Route::get('test', function () {
+    $numberToWords = new NumberToWords();
+    $numberTransformer = $numberToWords->getNumberTransformer('es'); // 'es' para español
 
-//     \Notification::send([], new App\Notifications\MovieTicketPaid());
-//     return "Test para whatsapp";
-// })->name('test_ws');
+    $texto = $numberTransformer->toWords(1934544621321656446); // Resultado: "mil doscientos treinta y cuatro"
+    echo $texto;
+})->name('test');
